ac7bd851cf1f92dfe217e2f53e4e326c79077f48
andamian <adrian.damian@nrc.ca>
2016-12-02 14:33:58 -0800

    Removed dependency on astropy in install (#4)
    * Removed dependency on astropy in install
    
    * Removed dependency on astropy in install - updated version number

--- cadcutils/cadcutils/_astropy_init.py
+++ cadcutils/cadcutils/_astropy_init.py
@@ -113,7 +113,6 @@ def test(package=None, test_path=None, args=None, plugins=None,
 if not _ASTROPY_SETUP_:
     import os
     from warnings import warn
-    from astropy import config
 
     # add these here so we only need to cleanup the namespace at the end
     config_dir = None
@@ -122,6 +121,7 @@ if not _ASTROPY_SETUP_:
         config_dir = os.path.dirname(__file__)
         config_template = os.path.join(config_dir, __package__ + ".cfg")
         if os.path.isfile(config_template):
+            from astropy import config
             try:
                 config.configuration.update_default_config(
                     __package__, config_dir, version=__version__)
