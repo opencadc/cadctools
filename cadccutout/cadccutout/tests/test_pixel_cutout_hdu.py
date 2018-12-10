22782e766ee809b1f6dce9f2adc92018fb52bd00
Dustin Jenkins <djenkins.cadc@gmail.com>
2018-12-10 14:09:00 -0800

    Hush the coverage.

--- cadccutout/cadccutout/tests/test_pixel_cutout_hdu.py
+++ cadccutout/cadccutout/tests/test_pixel_cutout_hdu.py
@@ -84,6 +84,12 @@ def test_create():
     test_subject = PixelCutoutHDU(extension='5')
     assert test_subject.get_extension() == 5, 'Wrong extension.'
 
+    try:
+      test_subject = PixelCutoutHDU([()])
+      assert False, 'Should throw ValueError as tuples are not long enough.'
+    except ValueError:
+      assert True, 'Good!'
+
 
 def test_get_shape():
     test_subject = PixelCutoutHDU([(1, 200), (305, 600)])
