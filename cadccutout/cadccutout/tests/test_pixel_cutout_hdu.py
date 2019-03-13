be9652e2672f98c755efc86a123369367cd26fef
Dustin <at88mph@users.noreply.github.com>
2019-03-13 13:37:49 -0700

    Small fixes for CADC integration tests to pass. (#91)

--- cadccutout/cadccutout/tests/test_pixel_cutout_hdu.py
+++ cadccutout/cadccutout/tests/test_pixel_cutout_hdu.py
@@ -82,6 +82,24 @@ def test_create():
     test_subject = PixelCutoutHDU([(1, 200), (30)], 'SCI,5')
     assert test_subject.get_extension() == ('SCI', 5), 'Wrong extension.'
 
+    test_subject = PixelCutoutHDU([(99, 101), (44, 66)], 'AMS ,2')
+    assert test_subject.get_extension() == ('AMS', 2), \
+        'Wrong extension (didn' 't filter out spaces)'
+
+    test_subject = PixelCutoutHDU([(12, 30)], 'EXT,0')
+    assert test_subject.get_extension() == ('EXT', 1)
+
+    with pytest.raises(ValueError) as ve:
+        test_subject = PixelCutoutHDU([(23, 67), (23, 89)],
+                                      extension='SCI, 3, i')
+    assert str(ve).index(
+        'Specifying XTENSION return type is not supported.') > 0
+
+    with pytest.raises(ValueError) as ve:
+        test_subject = PixelCutoutHDU([(23, 67, 25), (23, 89)],
+                                      extension='SCI, 3')
+    assert str(ve).index('ValueError: Invalid range ((23, 67, 25)).') > 0
+
     test_subject = PixelCutoutHDU(extension='5')
     assert test_subject.get_extension() == 5, 'Wrong extension.'
 
