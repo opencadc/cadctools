6d2c906133b2c444327d9acefa579a713762e553
Adrian Damian <Adrian.Damian@nrc.ca>
2019-08-16 13:53:14 -0700

    Completed transition away from Python3.4 and added support for Python3.7

--- cadctap/setup.py
+++ cadctap/setup.py
@@ -134,9 +134,6 @@ setup(name=PACKAGENAME,
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
       ],
       cmdclass={
           'coverage': PyTest, 'intTest': PyIntTest, 'allTest': PyAllTest
