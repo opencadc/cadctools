ce28c3a5b4f2eb2983bdfe4f4a2f5bb2d980b351
Dustin Jenkins <djenkins.cadc@gmail.com>
2019-11-15 10:01:31 -0800

    Hush the linter.

--- cadccutout/cadccutout/tests/test_utils.py
+++ cadccutout/cadccutout/tests/test_utils.py
@@ -70,12 +70,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-import logging
-import numpy as np
-
 import cadccutout.utils as u
-
-LOGGER = logging.getLogger('cadccutout')
 
 
 def test_to_num():
