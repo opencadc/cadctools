245e31b9e8ec8a1f0cea8154af20bad2f2af8f81
andamian <adrian.damian@nrc.ca>
2020-06-23 12:11:14 +1200

    Pass host arg in get_cert

--- cadcutils/cadcutils/net/auth.py
+++ cadcutils/cadcutils/net/auth.py
@@ -326,7 +326,7 @@ def get_cert_main():
 
     try:
         subject = Subject.from_cmd_line_args(args)
-        cert = get_cert(subject, days_valid=args.days_valid)
+        cert = get_cert(subject, days_valid=args.days_valid, host=args.host)
         with open(args.cert_filename, 'w') as w:
             w.write(cert)
         print('DONE. {} day certificate saved in {}'.format(
