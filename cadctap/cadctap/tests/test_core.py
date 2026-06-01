75c645afafce2dfd8c3c97a4f0e882d0edf1f558
Adrian Damian <Adrian.Damian@nrc.ca>
2026-06-01 16:27:03 -0700

    Propagated change to CLI

--- cadctap/cadctap/tests/test_core.py
+++ cadctap/cadctap/tests/test_core.py
@@ -756,15 +756,9 @@ class TestCadcTapClient(unittest.TestCase):
             assert usage.strip('\n') == _fix_help(stdout_mock.getvalue())
 
         # permission -h
-        # help is slightly different in python 3.9
-        if sys.version_info >= (3, 9):
-            with open(os.path.join(TESTDATA_DIR,
-                                   'help_permission_39.txt'), 'r') as myfile:
-                usage = myfile.read()
-        else:
-            with open(os.path.join(TESTDATA_DIR,
-                                   'help_permission.txt'), 'r') as myfile:
-                usage = myfile.read()
+        with open(os.path.join(TESTDATA_DIR,
+                               'help_permission.txt'), 'r') as myfile:
+            usage = myfile.read()
 
         with patch('sys.stdout', new_callable=StringIO) as stdout_mock:
             sys.argv = ['cadc-tap', 'permission', '--help']
